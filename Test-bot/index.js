2aaca1f7735c24f1ea77548d4173c7e1f9e32c75
jiyeong1004 <ak1157@naver.com>
2021-05-01 21:59:17 +0900

    #3 Token, massage

--- Test-bot/index.js
+++ Test-bot/index.js
@@ -6,9 +6,9 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === '야') {
+    msg.reply('호!');
   }
 });
 
-client.login('token');
+client.login('ODM4MDI5ODgxNzQ0Njg3MTE0.YI1KJQ.qzYv8IHR0aLFnvE9nQZWuq4nSeg');
